de675f11e29819e3d9184c5113f077d4f8eed99c
Irene Gu <gu.jiawen@northeastern.edu>
2024-09-19 00:08:17 -0700

    create route

--- inv-mgt-client/src/app/dashboard/page.tsx
+++ inv-mgt-client/src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import React from 'react'
 
 
 const Dashboard = () => {
   return (
-    <div>Dashboard
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             hello
         </main>
