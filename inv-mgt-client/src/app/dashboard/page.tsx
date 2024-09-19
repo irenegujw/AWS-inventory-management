fa78398fdf188bbaea794c382f436f97a49c5ead
Irene Gu <gu.jiawen@northeastern.edu>
2024-09-19 01:01:43 -0700

    Dashboard finish

--- inv-mgt-client/src/app/dashboard/page.tsx
+++ inv-mgt-client/src/app/dashboard/page.tsx
@@ -1,14 +1,79 @@
 "use client";
 
 import React from 'react'
+import CardPopularProducts from './CardPopularProducts';
+import CardPurchaseSummary from './CardPurchaseSummary';
+import CardExpenseSummary from './CardExpenseSummary';
+import CardSalesSummary from './CardSalesSummary';
+import StatCard from './StatCard';
+import { CheckCircle, Package, Tag, TrendingDown, TrendingUp } from 'lucide-react';
 
 
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            hello
-        </main>
+        <CardPopularProducts/>
+        <CardSalesSummary/>
+        <CardPurchaseSummary/>
+        <CardExpenseSummary/>
+        <StatCard
+        title="Customer & Expenses"
+        primaryIcon={<Package className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 September 2024"
+        details={[
+          {
+            title: "Customer Growth",
+            amount: "175.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Expenses",
+            amount: "10.00",
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+      <StatCard
+        title="Dues & Pending Orders"
+        primaryIcon={<CheckCircle className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 September 2024"
+        details={[
+          {
+            title: "Dues",
+            amount: "250.00",
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Pending Orders",
+            amount: "147",
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+      <StatCard
+        title="Sales & Discount"
+        primaryIcon={<Tag className="text-blue-600 w-6 h-6" />}
+        dateRange="22 - 29 September 2024"
+        details={[
+          {
+            title: "Sales",
+            amount: "1000.00",
+            changePercentage: 20,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: "Discount",
+            amount: "200.00",
+            changePercentage: -10,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+        
     </div>
   )
 }
